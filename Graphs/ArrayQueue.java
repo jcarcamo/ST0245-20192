8665666072beed123250b3c024896e330d409e18
Juan Gonzalo Carcamo Zuluaga <jcarcamo@gmail.com>
2019-10-01 06:57:49 -0500

    Graphs first commit

---
+++ Graphs/ArrayQueue.java
@@ -0,0 +1,27 @@
+public class ArrayQueue {
+    private final int SIZE = 20;
+    private int[] queArray;
+    private int front;
+    private int rear;
+    public ArrayQueue(){
+        queArray = new int[SIZE];
+        front = 0;
+        rear = -1;
+    }
+    public void insert(int j){ // put item at rear of queue
+        if(rear == SIZE-1)
+            rear = -1;
+        queArray[++rear] = j;
+    }
+
+    public int remove(){ // take item from front of queue
+        int temp = queArray[front++];
+        if(front == SIZE)
+            front = 0;
+        return temp;
+    }
+
+    public boolean isEmpty(){ // true if queue is empty
+        return ( rear+1==front || (front+SIZE-1==rear) );
+    }
+}
---
+++ Graphs/ArrayStack.java
@@ -0,0 +1,25 @@
+public class ArrayStack {
+    private final int SIZE = 20;
+    private int[] st;
+    private int top;
+
+    public ArrayStack(){
+        st = new int[SIZE]; // make array
+        top = -1;
+    }
+    public void push(int j){ // put item on stack
+        st[++top] = j;
+    }
+
+    public int pop(){  // take item off stack
+        return st[top--];
+    }
+
+    public int peek(){ // peek at top of stack
+        return st[top];
+    }
+
+    public boolean isEmpty(){ // true if nothing on stack
+        return (top == -1);
+    }
+}
